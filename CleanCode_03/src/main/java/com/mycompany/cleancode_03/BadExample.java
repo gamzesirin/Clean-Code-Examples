993406a5d143c22f25e020e81f38173fcd601893
Gamze Şirin <gamze1sirin@gmail.com>
2024-11-21 15:40:42 +0300

    feat:add clean code example

---
+++ CleanCode_03/src/main/java/com/mycompany/cleancode_03/BadExample.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.cleancode_03;
+
+import java.util.Date;
+
+/**
+ *
+ * @author gamze
+ */
+public class BadExample {
+    
+    private Date genymdhms;
+    private Date modymdhms;
+    private final String pszqint = "102";
+
+    private String ronaldo;
+}
---
+++ CleanCode_03/src/main/java/com/mycompany/cleancode_03/GoodExample.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.cleancode_03;
+
+/**
+ *
+ * @author gamze
+ */
+import java.util.Date;
+public class GoodExample {
+    private Date generationTimestamp;
+    private Date modificationTimestamp;;
+    private final String recordId = "102";
+}
