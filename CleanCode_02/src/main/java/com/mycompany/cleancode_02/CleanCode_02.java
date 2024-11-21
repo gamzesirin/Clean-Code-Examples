b439fe70d0962514f2230d97de22fdb891283379
Gamze Şirin <gamze1sirin@gmail.com>
2024-11-21 15:40:33 +0300

    feat:add clean code example

---
+++ CleanCode_02/src/main/java/com/mycompany/cleancode_02/CleanCode_02.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cleancode_02;
+
+/**
+ *
+ * @author gamze
+ */
+public class CleanCode_02 {
+
+    String accountName = "";
+
+
+    static int O;
+    static int l;
+
+    public static void main(String[] args) {
+        {
+            int a = l;
+            if ( O == l )
+                a = 1;
+            else
+                l = 01;
+        }
+
+        System.out.println(l);
+    }
+}
