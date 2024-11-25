297a8f028d8943a265e2b5b5b1023ce5ab99bf26
Gamze Şirin <gamze1sirin@gmail.com>
2024-11-25 13:14:21 +0300

    feat:add clean code example

---
+++ CleanCode_06/src/main/java/com/mycompany/cleancode_06/CleanCode_06.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cleancode_06;
+
+/**
+ *
+ * @author gamze
+ */
+public class CleanCode_06 {
+
+  
+        /*
+        String schoolName;
+        BigDecimal moneyToRegister;
+        int capacity;
+        int rank;
+     */
+    public static void register(School school) {
+
+
+    }
+    
+}
---
+++ CleanCode_06/src/main/java/com/mycompany/cleancode_06/School.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.cleancode_06;
+
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author gamze
+ */
+public class School {
+
+    String schoolName;
+    BigDecimal moneyToRegister;
+    int capacity;
+    int rank;
+
+
+}
