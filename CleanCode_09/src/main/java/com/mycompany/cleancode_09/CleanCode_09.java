1d99fb2ddb7f395d3f0707b90a6f032a948a35c3
Gamze Şirin <gamze1sirin@gmail.com>
2024-11-27 08:55:53 +0300

    feat: add clean code example

---
+++ CleanCode_09/src/main/java/com/mycompany/cleancode_09/CleanCode_09.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cleancode_09;
+
+/**
+ *
+ * @author gamze
+ */
+public class CleanCode_09 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ CleanCode_09/src/main/java/com/mycompany/cleancode_09/Error.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.cleancode_09;
+
+/**
+ *
+ * @author gamze
+ */
+    public enum Error {
+    // DEPENDENCY MAGNET
+    OK,
+    INVALID,
+    NO_SUCH,
+    LOCKED,
+    OUT_OF_RESOURCES,
+    WAITING_FOR_EVENT;
+}
