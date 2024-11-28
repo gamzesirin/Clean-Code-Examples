3e2bc46077b7e7e9cf23b7d7cfbb3d43baeefd31
Gamze Şirin <gamze1sirin@gmail.com>
2024-11-28 22:33:49 +0300

    feat: add clean code example

---
+++ CleanCode_10/src/main/java/com/mycompany/cleancode_10/CleanCode_10.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cleancode_10;
+
+/**
+ *
+ * @author gamze
+ */
+public class CleanCode_10 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
